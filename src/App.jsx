df18208b7103fa82cec82fdd0c84d3f8b0532e79
ShumpeiYokoi <yokoi@cloudbuilders.jp>
2025-01-08 21:31:01 +0900

    clear react3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 // DO NOT DELETE
 
 import './App.css'
+import { useState } from 'react'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg')
   return (
     <div>
-      <header>Dogアプリ</header>
+      <header>
+        <h1>Dogアプリ</h1>
+        <p>このサイトは可愛い犬の写真を紹介します</p>
+      </header>
+      <img src={dogUrl} alt="cute dog"/>
     </div>
   )
 }
