c876016a016a496e6047ab4512ae511f2b26ef19
ShumpeiYokoi <yokoi@cloudbuilders.jp>
2025-01-11 11:26:53 +0900

    clear react7

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export const App = () => {
 
   return (
     <div>
-      <header>
+      <header className='app-header'>
         <h1>Dogアプリ</h1>
         <p>このサイトは可愛い犬の写真を紹介します</p>
       </header>
