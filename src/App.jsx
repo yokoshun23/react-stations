6b594a705beb9f90d470603d0d415b5d16358f49
ShumpeiYokoi <yokoi@cloudbuilders.jp>
2025-01-11 11:22:10 +0900

    clear react6

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,14 @@ import { useState } from 'react'
  * @type {() => JSX.Element}
  */
 export const App = () => {
-  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg')
+  const [dogUrl, setDogUrl] = useState('')
 
-  const changeDogImage = () => setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')
+  // 犬の画像を取得する関数
+  const updateDogImage = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => setDogUrl(data.message))
+  }
 
   return (
     <div>
@@ -18,7 +23,7 @@ export const App = () => {
         <p>このサイトは可愛い犬の写真を紹介します</p>
       </header>
       <img src={dogUrl} alt="cute dog"/>
-      <button onClick={changeDogImage}>更新</button>
+      <button onClick={updateDogImage}>更新</button>
     </div>
   )
 }
