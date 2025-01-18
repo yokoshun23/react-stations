25500861cb1e11655e475a5ad57d9153b3c52819
ShumpeiYokoi <yokoi@cloudbuilders.jp>
2025-01-19 08:03:41 +0900

    clear react9

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import './App.css'
 import { Header } from './Header'
 import { Description } from './Description'
+import DogListContainer from './DogListContainer'
 
 /**
  * @type {() => JSX.Element}
@@ -12,6 +13,7 @@ export const App = () => {
     <React.Fragment>
       <Header />
       <Description />
+      <DogListContainer />
     </React.Fragment>
   )
 }
--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,7 +1,26 @@
-// @ts-check
+import { useState, useEffect } from "react"
 
 export const DogListContainer = () => {
-  return <></>
+  const [breeds, setBreeds] = useState([])
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then((res) => res.json())
+      .then((data) => {
+        const breedList = Object.keys(data.message)
+        setBreeds(breedList)
+      })
+  }, [])
+  return (
+    <div>
+      <h2>犬の品種一覧</h2>
+      <ul>
+        {breeds.map((breed) => (
+          <li key={breed}>{breed}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default DogListContainer
